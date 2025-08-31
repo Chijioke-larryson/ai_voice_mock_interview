ae6395de4fbdde8de587dc91ae0f65b84a4b8c1a
SilentXBT <larrysongift@gmail.com>
2025-08-31 16:15:26 -0700

    Auto-commit at 2025-08-31T23:15:26.436Z

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -55,6 +55,15 @@ const AuthForm = ({type}:{form: FormType}) => {
                 </form>
             </Form>
 
+                <p className="text-center">
+                    {isSignIn ? 'No account yet': 'Have an account already'}
+                    <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className=" font-bold text-user-primary ml-1">
+                        {!isSignIn ? "Sign in" : "Sign up"}
+                    </Link>
+
+
+                </p>
+
         </div>
         </div>
 
